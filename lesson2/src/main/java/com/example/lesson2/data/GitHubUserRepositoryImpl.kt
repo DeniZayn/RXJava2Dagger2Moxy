2222c9cb26100af532d5f0bf1ceb8b274c9cbea7
DeniZayn <0990759@gmail.com>
2021-12-12 10:50:50 +0300

    HW for lesson2

---
+++ lesson2/src/main/java/com/example/lesson2/data/GitHubUserRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.example.lesson2.data
+
+class GitHubUserRepositoryImpl : GitHubUserRepository {
+
+    private val users = listOf(
+        GitHubUser("login1"),
+        GitHubUser("login2"),
+        GitHubUser("login3"),
+        GitHubUser("login4"),
+        GitHubUser("login5"),
+    )
+
+    override fun getUsers() = users
+
+    override fun getUserByLogin(userId: String): GitHubUser? =
+        users.firstOrNull { user -> user.login == userId }
+}
